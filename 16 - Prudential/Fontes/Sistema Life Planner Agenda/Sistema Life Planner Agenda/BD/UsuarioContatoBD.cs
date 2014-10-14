327db970bae18888c9a7921befcd00666e5b8845
cristian.co.gomes@gmail.com <cristian.co.gomes@gmail.com@79c8f0f7-7590-ac12-b88a-f8bd86be65ff>
2014-10-14 01:34:25 +0000

    Conclusão da alteração de contato (banco); seguir Criando os links entre contatos e usuários;

---
+++ 16 - Prudential/Fontes/Sistema Life Planner Agenda/Sistema Life Planner Agenda/BD/UsuarioContatoBD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_Life_Planner_Agenda.BD
+{
+    public class UsuarioContatoBD : BancoBase
+    {
+        public void Incluir(
+            int ID_Contato,
+            int ID_Usuario,
+            DateTime Data_Cadastro)
+        {
+            comando.CommandText = @"INSERT INTO usuario_contato
+                                        (ID_Contato, ID_Usuario, Data_Cadastro) 
+                                    VALUES (@ID_Contato, @ID_Usuario, @Data_Cadastro);";
+            comando.Parameters.AddWithValue("@ID_Contato", ID_Contato);
+            comando.Parameters.AddWithValue("@Data_Cadastro", Data_Cadastro);
+            comando.Parameters.AddWithValue("@ID_Usuario", ID_Usuario);
+
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.ExecuteNonQuery();
+        }
+    }
+}
