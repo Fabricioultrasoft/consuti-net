53171a127e09069b877a53c8d6f83faed275d3eb
cristian.co.gomes@gmail.com <cristian.co.gomes@gmail.com@79c8f0f7-7590-ac12-b88a-f8bd86be65ff>
2014-12-15 11:40:16 +0000

    Agenda do home concluída

--- 16 - Prudential/Fontes/Sistema Life Planner Agenda/Sistema Life Planner Agenda/Default.aspx.cs
+++ 16 - Prudential/Fontes/Sistema Life Planner Agenda/Sistema Life Planner Agenda/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Sistema_Life_Planner_Agenda.Classes;
+using Sistema_Life_Planner_Agenda.BD;
 
 namespace Sistema_Life_Planner_Agenda.Site
 {
@@ -12,7 +13,11 @@ namespace Sistema_Life_Planner_Agenda.Site
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                ValidaUserLogado();
+                CarregarGridView();
+            }
         }
 
         protected void NovoContatoImageButton_Click(object sender, ImageClickEventArgs e)
@@ -54,5 +59,51 @@ namespace Sistema_Life_Planner_Agenda.Site
         {
             Response.Redirect("SITPLAN/CadastrarSITPLAN.aspx");
         }
+
+        private void CarregarGridView()
+        {
+            string idContato = string.Empty;
+
+            DateTime dataDe = DateTime.Now;
+            DateTime dataAte = DateTime.Now.AddDays(7);
+
+            AgendaGridView.DataSource = new AgendaBD().Listar(
+                idContato,
+                Convert.ToDateTime(dataDe),
+                Convert.ToDateTime(dataAte),
+                Convert.ToInt32(new UsuarioBD().ObterID(Session["emailUsuarioLogado"].ToString())));
+            AgendaGridView.DataBind();
+        }
+
+        #region Eventos do GridView
+
+        /// <summary>
+        /// Exclui ou altera um registro no grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void AgendaGridView_Click(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName == "Excluir")
+                {
+                    new AgendaBD().Excluir(Convert.ToInt32(e.CommandArgument));
+                    ExibeMensagemPopUp("Compromisso removido com sucesso!");
+                    CarregarGridView();
+                }
+                else if (e.CommandName == "Alterar")
+                {
+                    // o ",false" é para suprimir a chamada interna para "Response.End"
+                    Response.Redirect("~/Agenda/CadastrarAgenda.aspx?idAgenda=" + Convert.ToString(e.CommandArgument), false);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExibeMensagemPopUp("Erro interno. Tente novamente mais tarde. Detalhes: " + ex.Message);
+            }
+        }
+
+        #endregion
     }
 }
